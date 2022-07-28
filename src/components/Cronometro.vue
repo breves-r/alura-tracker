55364c22be6ef93782594a2f21f498bbe066a2b3
Rafaela Breves <rafaela.curty13@gmail.com>
2022-07-28 14:47:29 -0300

    first commit

---
+++ src/components/Cronometro.vue
@@ -0,0 +1,24 @@
+<template>
+    <section>
+        <strong>{{tempoDecorrido}}</strong>
+    </section>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue'
+
+    export default defineComponent({
+        name: 'Cronometro',
+        props: {
+            tempoTotal: {
+                type: Number,
+                default: 0
+            }
+        },
+        computed: {
+            tempoDecorrido() : string{
+                return new Date(this.tempoTotal * 1000).toISOString().substr(11,8)
+            }
+        }
+    })
+</script>
